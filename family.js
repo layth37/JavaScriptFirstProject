1e2af4634c7f6a197402c5c4c37897a05f5bc02f
layth37 <85255203+layth37@users.noreply.github.com>
2021-06-20 22:42:57 -0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,56 @@
+// let homeDisplay = ["hall.jpg", "food.jpg", "flower.jpg", "clothes.jpg"];
+
+// let cHome = document.querySelector('.containerHome');
+
+// const homeDisplayPhoto = document.createElement('img');
+// homeDisplayPhoto.setAttribute('class', 'homeDisplayPh');
+// homeDisplayPhoto.src = "hall.jpg";
+// document.body.appendChild(homeDisplayPhoto);
+
+const para = document.createElement('p');
+document.body.appendChild(para);
+para.textContent = 'Value: ';
+
+
+
+
+
+// const homeDisplayPara = document.createElement('p');
+// homeDisplayPara.setAttribute('class', 'homeDisplayPara');
+// homeDisplayPara.textContent="fuck";
+// cHome.appendChild(homeDisplayPara);
+// body.appendChild(homeDisplayPara);
+
+
+
+
+// const HomeDisplayBtn = document.querySelector('.leftBtn');
+
+// function add_img() { 
+// 	var img = document.createElement('img'); 
+//     img.src = 'hall.png'; 
+// 	document.getElementById('body').appendChild(img);
+// }
+
+// add_img();
+
+// const btn = document.querySelector('button');
+
+// function random(number) {
+//     return Math.floor(Math.random() * number);
+// }
+
+// function bgChange(e) {
+//     const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+//     e.target.style.backgroundColor = rndCol;
+//     console.log(e);
+// }
+
+// btn.addEventListener('click', bgChange);
+
+// alert("f");
+
+
+
+
+//HomeDisplayBtn.addEventListener(click, );
